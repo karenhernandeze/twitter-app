8648c1bd32afaaa30c52abd2f01240866b88cabb
santiadlv <49079265+santiadlv@users.noreply.github.com>
2023-05-31 19:27:11 -0600

    Create CRUD operations for replies table

---
+++ twitter-reloaded/backend/src/crud/replies_crud.py
@@ -0,0 +1,25 @@
+from ..database.config import TwitterDB as db
+
+
+class RepliesCRUD:   
+    def create_reply(parentId: int, replyId: int):
+        query = """
+        INSERT INTO replies (parentId, replyId)
+        VALUES (?, ?)
+        RETURNING *;
+        """
+        values = (parentId, replyId)
+        row = db.execute_query_values(query, values)
+        return row
+    
+    def get_tweet_replies(tweetId: int):
+        query = """
+        SELECT t.* 
+        FROM tweets as t
+        JOIN replies as r ON t.tweetId = r.replyId
+        WHERE r.parentId = ?
+        ORDER BY t.createdAt DESC;
+        """
+        values = (tweetId,)
+        rows = db.execute_query_values_all(query, values)
+        return rows
